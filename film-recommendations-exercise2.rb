3d5ff63e3461da3cef50958dc3d3698b66aa2d58
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-19 13:13:39 -0400

    Recommend a drama or comedy

--- film-recommendations-exercise2.rb
+++ film-recommendations-exercise2.rb
@@ -2,6 +2,7 @@ recommended_documentary = "Tim's Vermeer"
 recommended_drama = "The Departed"
 recommended_comedy = "Hot Fuzz"
 recommended_dramedy = "The Way Way Back"
+recommended_book = "American Gods"
 
 puts "On a scale of 1 - 5..."
 puts "Please rate your appreciation for documentaries"
@@ -17,4 +18,9 @@ if documentary_rate > 4
   puts "You should watch #{recommended_documentary}."
 elsif documentary_rate < 3 && (drama_rate > 4 && comedy_rate > 4)
   puts "You should watch #{recommended_dramedy}."
+elsif drama_rate > 4 && (documentary_rate < 4 && comedy_rate < 4)
+  puts "You should watch #{recommended_drama}."
+elsif comedy_rate > 4 && (documentary_rate < 4 && drama_rate < 4)
+  puts "You should watch #{recommended_comedy}."
+
 end
