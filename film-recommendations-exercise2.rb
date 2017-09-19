c2fb0ea8c1b3cdd25687cfa6cdb236fcf88154c3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-19 13:04:46 -0400

    Ask to rate genres

---
+++ film-recommendations-exercise2.rb
@@ -0,0 +1,14 @@
+recommended_documentary = "Tim's Vermeer"
+recommended_drama = "The Departed"
+recommended_comedy = "Hot Fuzz"
+recommended_dramedy = "The Way Way Back"
+
+puts "On a scale of 1 - 5..."
+puts "Please rate your appreciation for documentaries"
+documentary_anwser = gets.chomp.to_i
+puts "Please rate your appreciation for dramas"
+drama_anwser = gets.chomp.to_i
+puts "Please rate your appreciation for comedies"
+comedy_anwser = gets.chomp.to_i
+puts "Please rate your appreciation for dramedies"
+dramedy_anwser = gets.chomp.to_i
