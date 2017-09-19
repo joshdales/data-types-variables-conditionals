0763194bae755fb16af0f1dab3ffe648547ef490
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-19 12:48:34 -0400

    Recommend a documentary

--- film-recommendations.rb
+++ film-recommendations.rb
@@ -2,3 +2,12 @@ recommended_documentary = "Tim's Vermeer"
 recommended_drama = "The Departed"
 recommended_comedy = "Hot Fuzz"
 recommended_dramedy = "The Way Way Back"
+
+puts "Do you enojoy documentaries?"
+documentary_anwser = gets.chomp
+if documentary_anwser == "yes"
+  puts "You should watch #{recommended_documentary}."
+  return
+else
+  puts "Please anwser yes or no"
+end
